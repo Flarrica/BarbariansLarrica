d184021c72f38bbd32afc0cac1495700e1943f12
Flarrica <107935237+Flarrica@users.noreply.github.com>
2022-09-22 01:38:30 -0300

    mas js tienda y nosotros

---
+++ js/nosotros.js
@@ -0,0 +1,108 @@
+const jugadores = [
+    {
+        id: 1,
+        numero: 38,
+        nombre: 'Fernando Larrica',
+        posicion: 'CT/NT/FB/LB/QB',
+        img: '../assets/img/FernandoLarrica.jpg'
+    },
+    {
+        id: 2,
+        numero: 06,
+        nombre: 'Fabian Hernandez',
+        posicion: 'SS',
+        img: '../assets/img/MVP4.jpg'
+    },
+    {
+        id: 3,
+        numero: 85,
+        nombre: 'Gabriel Sanchez',
+        posicion: 'WR',
+        img: '../assets/img/MVP3.jpeg'
+    },
+    {
+        id: 4,
+        numero: 26,
+        nombre: 'Luciano Guillen',
+        posicion: 'RB',
+        img: '../assets/img/MVP1.jpg'
+    },
+    {
+        id: 5,
+        numero: 02,
+        nombre: 'Luciano Guillen',
+        posicion: 'RB',
+        img: '../assets/img/MVP2.jpg'
+    },
+    {
+        id: 6,
+        numero: 01,
+        nombre: 'Felipe Poggio',
+        posicion: 'Head Coach',
+        img: '../assets/img/HeadCouch.jpg'
+    }
+];
+const jugadoresMVP = [
+
+    {
+        id: 1,
+        numero: 06,
+        nombre: 'Fabian Hernandez',
+        posicion: 'SS',
+        img: '../assets/img/MVP4.jpg'
+    },
+    {
+        id: 2,
+        numero: 85,
+        nombre: 'Gabriel Sanchez',
+        posicion: 'WR',
+        img: '../assets/img/MVP3.jpeg'
+    },
+    {
+        id: 3,
+        numero: 26,
+        nombre: 'Luciano Guillen',
+        posicion: 'RB',
+        img: '../assets/img/MVP1.jpg'
+    },
+];
+const trofeos = [
+    {
+        id: 1,
+        nombre: 'Trofeo 2019',
+        img: '../assets/img/trofeo2019.jpeg'
+    },
+    {
+        id: 3,
+        nombre: 'Trofeo 2018 en memoria de "Daniel Bilche"',
+        img: '../assets/img/trofeoDAnielBilche.jpeg'
+    },
+    {
+        id: 4,
+        nombre: 'Trofeo 2018<',
+        img: '../assets/img/trofeoApertura2018.jpeg'
+    },
+    {
+        id: 5,
+        nombre: 'Trofeo Clausura 2016',
+        img: '../assets/img/trofeoClausura2016.jpeg'
+    },
+];
+
+const cards = document.getElementById('tarjetas');
+
+jugadores.forEach(jugador =>{
+
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+        <div data-aos="fade-up" class="card border-success m-2" style="width: 18rem;">
+            <img src="${jugador.img}" class="card-img-top">
+                <div class="card-body">
+                    <p class="card-text">${jugador.numero} - ${jugador.nombre}</p>
+                    <p class="card-text">${jugador.posicion}</p>
+                </div>
+        </div>
+    `;
+    cards.appendChild(div);
+});
--- js/tienda.js
+++ js/tienda.js
@@ -46,3 +46,65 @@ btnUsuario.addEventListener('click', () => {
 
     
 });
+
+const stocks = [
+    {
+        id: 1,
+        nombre: 'Gorra Negra',
+        precio: 200,
+        img: '../assets/img/GorroN.jpg',
+        talles: false
+    },
+    {
+        id: 2,
+        nombre: 'Gorra Verde',
+        precio: 200,
+        img: '../assets/img/GorroV.jpg',
+        talles: false
+    },
+    {
+        id: 3,
+        nombre: 'Remera Gris',
+        precio: 250,
+        img: '../assets/img/RemeraG.jpg',
+        talles: true
+    },
+    {
+        id: 4,
+        nombre: 'Canguro Gris',
+        precio: 500,
+        img: '../assets/img/CanguroG.jpg',
+        talles: true
+    },
+    {
+        id: 5,
+        nombre: 'Remera Flag',
+        precio: 300,
+        img: '../assets/img/RemeraFlag.jpg',
+        talles: true
+    },
+];
+
+
+
+
+
+
+const stock = document.getElementById('mercaderia');
+
+stocks.forEach(stoc =>{
+
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+        <div class="card m-2" style="width: 18rem;">
+            <img src="${stoc.img}" class="card-img-top">
+            <div class="card-body">
+                <h4 class="card-title">${stoc.nombre}</h4>
+                <p class="card-text">Talles $${stoc.precio} .</p>
+                <a href="#" class="btn btnForm">Comprar</a>
+            </div>
+        </div>
+    `;
+    stock.appendChild(div);
+});
