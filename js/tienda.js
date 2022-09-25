2b908ef590cb004e81985d6f4b66946482770bd6
Flarrica <107935237+Flarrica@users.noreply.github.com>
2022-09-25 17:02:21 -0300

    agregué carrito en tienda

--- js/tienda.js
+++ js/tienda.js
@@ -1,4 +1,5 @@
 // iniciar usuario
+const d = document;
 
 class Usuario {
     constructor(nombre,contraseña,iniciado){
@@ -10,7 +11,7 @@ class Usuario {
 const usuarios = [];
 
 
-const btnUsuario = document.querySelector('.ingresarUsuario');
+const btnUsuario = d.querySelector('.ingresarUsuario');
 
 btnUsuario.addEventListener('click', () => {
     let check = true;
@@ -28,14 +29,14 @@ btnUsuario.addEventListener('click', () => {
 
         console.log(usuarios);
 
-        let name = document.querySelector('.usuario');
+        let name = d.querySelector('.usuario');
         name.textContent = nombre;
-        let cerrarSesion = document.querySelector('.ingresarUsuario');
+        let cerrarSesion = d.querySelector('.ingresarUsuario');
         cerrarSesion.innerHTML = 'Cerrar Sessión';
     }else if (check == false){
-        let name = document.querySelector('.usuario');
+        let name = d.querySelector('.usuario');
         name.textContent = '';
-        let cerrarSesion = document.querySelector('.ingresarUsuario');
+        let cerrarSesion = d.querySelector('.ingresarUsuario');
         cerrarSesion.innerHTML = 'Iniciar Sesión';    
         usuarios.forEach(usuario =>{
             if (usuario.iniciado == true) {
@@ -48,12 +49,13 @@ btnUsuario.addEventListener('click', () => {
     
 });
 
-const stocks = [
+const BBDD = [
     {
         id: 1,
         nombre: 'Gorra Negra',
         precio: 200,
         img: '../assets/img/GorroN.jpg',
+        cantidad: 0,
         talles: false
     },
     {
@@ -61,6 +63,7 @@ const stocks = [
         nombre: 'Gorra Verde',
         precio: 200,
         img: '../assets/img/GorroV.jpg',
+        cantidad: 0,
         talles: false
     },
     {
@@ -68,6 +71,7 @@ const stocks = [
         nombre: 'Remera Gris',
         precio: 250,
         img: '../assets/img/RemeraG.jpg',
+        cantidad: 0,
         talles: true
     },
     {
@@ -75,6 +79,7 @@ const stocks = [
         nombre: 'Canguro Gris',
         precio: 500,
         img: '../assets/img/CanguroG.jpg',
+        cantidad: 0,
         talles: true
     },
     {
@@ -82,30 +87,120 @@ const stocks = [
         nombre: 'Remera Flag',
         precio: 300,
         img: '../assets/img/RemeraFlag.jpg',
+        cantidad: 0,
         talles: true
     },
 ];
 
 
 
+const carrito = [];
 
+function renderProductos(){
 
+    const stock = d.getElementById('mercaderia');
 
-const stock = document.getElementById('mercaderia');
+    BBDD.forEach((p) =>{
 
-stocks.forEach(stoc =>{
+        let producto = d.createElement('div');
+        producto.classList.add('col-12','col-md-3','mb-5', 'd-flex','justify-content-center');
 
-    const div = document.createElement('div');
-    div.classList.add('card');
-    div.innerHTML = `
-        <div class="card m-2" style="width: 18rem;">
-            <img src="${stoc.img}" class="card-img-top">
-            <div class="card-body">
-                <h4 class="card-title">${stoc.nombre}</h4>
-                <p class="card-text">Talles $${stoc.precio} .</p>
-                <a href="#" class="btn btnForm">Comprar</a>
+        producto.innerHTML = `
+            <div class="card m-2" style="width: 18rem;">
+                <img src="${p.img}" class="card-img-top">
+                <div class="card-body">
+                    <h4 class="card-title">${p.nombre}</h4>
+                    <p class="card-text">Talles $${p.precio} .</p>
+                    <button class="btn btnForm" id= "${p.id}">Añadir al carrito</button>
+                </div>
             </div>
-        </div>
-    `;
-    stock.appendChild(div);
-});
+        `;
+        stock.appendChild(producto);
+
+        producto.querySelector('button').addEventListener('click', ()=>{
+        
+            agregarCompraCarrito(p.id);
+            
+        })
+    })
+};
+
+renderProductos();
+
+function agregarCompraCarrito(id){
+
+    let producto = BBDD.find(producto => producto.id === id);
+
+    let productosEnCarrito = carrito.find(producto => producto.id === id);
+
+    if (productosEnCarrito) {
+        
+        productosEnCarrito.cantidad++;
+
+    }else {
+        producto.cantidad = 1;
+        carrito.push(producto);
+    }
+
+    renderCarrito();
+    precioFinal();
+};
+
+function renderCarrito(){
+
+    let carritoHTML = d.querySelector('#carrito');
+    
+    carritoHTML.innerHTML='';
+
+    carrito.forEach((p , index)=>{
+        let producto = d.createElement('div');
+        producto.classList.add('card');
+        producto.innerHTML = `
+            <div class="card m-2" style="width: 18rem;">
+                <img src="${p.img}" class="card-img-top">
+                <div class="card-body">
+                    <h4 class="card-title">${p.nombre}</h4>
+                    <p class="card-text">Talles $${p.precio}</p>
+                    <p class="card-text">Cantidad ${p.cantidad}</p>
+                    <button class="btn btnForm" id= "${p.id}">Eliminar</button>
+                </div>
+            </div>
+        `;
+
+        producto.querySelector('button').addEventListener('click',()=>{
+
+            eliminarProductoCarrito(index);
+
+        });
+
+        carritoHTML.appendChild(producto);
+
+    })
+
+}
+
+function eliminarProductoCarrito(indice){
+
+    carrito[indice].cantidad--;
+
+    if (carrito[indice].cantidad === 0) {
+
+        carrito.splice(indice,1);
+        
+    };
+
+    renderCarrito();
+    precioFinal();
+}
+
+function precioFinal(){
+
+    let precioFinal = 0;
+    carrito.forEach((p)=>{
+
+        precioFinal += p.precio * p.cantidad;
+
+    });
+    const pagar = d.getElementById('totalPagar');
+    pagar.innerHTML=`<h4>Total a Pagar: $${precioFinal}</h4>`;
+}
