b15bca16b88509e65eaa97a3824430d2df1a0168
Flarrica <107935237+Flarrica@users.noreply.github.com>
2022-09-26 15:58:14 -0300

    BBDD

---
+++ js/BBDD.js
@@ -0,0 +1,134 @@
+const BBDD = [
+    {
+        id: 1,
+        nombre: 'Gorra Negra',
+        precio: 200,
+        img: '../assets/img/GorroN.jpg',
+        cantidad: 0,
+        talles: false
+    },
+    {
+        id: 2,
+        nombre: 'Gorra Verde',
+        precio: 200,
+        img: '../assets/img/GorroV.jpg',
+        cantidad: 0,
+        talles: false
+    },
+    {
+        id: 3,
+        nombre: 'Remera Gris',
+        precio: 250,
+        img: '../assets/img/RemeraG.jpg',
+        cantidad: 0,
+        talles: true
+    },
+    {
+        id: 4,
+        nombre: 'Canguro Gris',
+        precio: 500,
+        img: '../assets/img/CanguroG.jpg',
+        cantidad: 0,
+        talles: true
+    },
+    {
+        id: 5,
+        nombre: 'Remera Flag',
+        precio: 300,
+        img: '../assets/img/RemeraFlag.jpg',
+        cantidad: 0,
+        talles: true
+    },
+];
+
+
+const jugadores = [
+    {
+        id: 1,
+        numero: 38,
+        nombre: 'Fernando Larrica',
+        posicion: 'CT/NT/FB/LB/QB',
+        img: '../assets/img/FernandoLarrica.jpg'
+    },
+    {
+        id: 2,
+        numero: 06,
+        nombre: 'Fabian Hernandez',
+        posicion: 'SS',
+        img: '../assets/img/MVP4.jpg'
+    },
+    {
+        id: 3,
+        numero: 85,
+        nombre: 'Gabriel Sanchez',
+        posicion: 'WR',
+        img: '../assets/img/MVP3.jpeg'
+    },
+    {
+        id: 4,
+        numero: 26,
+        nombre: 'Luciano Guillen',
+        posicion: 'RB',
+        img: '../assets/img/MVP1.jpg'
+    },
+    {
+        id: 5,
+        numero: 02,
+        nombre: 'Luciano Guillen',
+        posicion: 'RB',
+        img: '../assets/img/MVP2.jpg'
+    },
+    {
+        id: 6,
+        numero: 01,
+        nombre: 'Felipe Poggio',
+        posicion: 'Head Coach',
+        img: '../assets/img/HeadCouch.jpg'
+    },
+];
+const jugadoresMVP = [
+
+    {
+        id: 1,
+        numero: 06,
+        nombre: 'Fabian Hernandez',
+        posicion: 'SS',
+        img: '../assets/img/MVP4.jpg'
+    },
+    {
+        id: 2,
+        numero: 85,
+        nombre: 'Gabriel Sanchez',
+        posicion: 'WR',
+        img: '../assets/img/MVP3.jpeg'
+    },
+    {
+        id: 3,
+        numero: 26,
+        nombre: 'Luciano Guillen',
+        posicion: 'RB',
+        img: '../assets/img/MVP1.jpg'
+    },
+];
+const trofeos = [
+    {
+        id: 1,
+        nombre: 'Trofeo 2019',
+        img: '../assets/img/trofeo2019.jpeg'
+    },
+    {
+        id: 3,
+        nombre: 'Trofeo 2018 en memoria de "Daniel Bilche"',
+        img: '../assets/img/trofeoDAnielBilche.jpeg'
+    },
+    {
+        id: 4,
+        nombre: 'Trofeo 2018<',
+        img: '../assets/img/trofeoApertura2018.jpeg'
+    },
+    {
+        id: 5,
+        nombre: 'Trofeo Clausura 2016',
+        img: '../assets/img/trofeoClausura2016.jpeg'
+    },
+];
